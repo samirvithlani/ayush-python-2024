a98ef5063a1761f8e430e3678d1f9070b66fbae7
samirvithlani <samir.vithlani83955@gmail.com>
2024-01-01 11:36:15 +0530

    basics

---
+++ basics/basci1.py
@@ -0,0 +1,27 @@
+print("Hello World") 
+age=18
+print(age)
+print("age = ",age)
+print("type of age =",type(age))    
+
+#int  8 bytes# float ,str,bool ,None
+
+per = 78.9
+print("per = ",per)
+print("type of per =",type(per))
+
+name = "Ram"
+print("name = ",name)
+print("type of name =",type(name))
+
+choice = 'Y'
+print("choice = ",choice)
+print("type of choice =",type(choice))
+
+isActive = True
+print("isActive = ",isActive)
+print("type of isActive =",type(isActive))
+
+person = None
+print("person = ",person)
+print("type of person =",type(person))
