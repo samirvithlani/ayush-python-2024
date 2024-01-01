f85371f7cefadbc21529dea5b325f990d07de112
samirvithlani <samir.vithlani83955@gmail.com>
2024-01-01 11:54:03 +0530

    input function

---
+++ basics/inputdemo.py
@@ -0,0 +1,22 @@
+#scanf,scanner, input function...
+#input function return type string...
+
+# no1 = input("Enter no1 : ")
+# print("no1 = ",no1+100) #"101"
+# print("type of no1 =",type(no1))
+# int a = 100.10
+# a = 100
+
+
+no1 = int(input("Enter no1 : ")) #explicit type casting
+print("no1 = ",no1+100) 
+print("type of no1 =",type(no1))
+
+per = float(input("Enter per : "))
+print("per = ",per)
+print("type of per =",type(per))
+
+
+name =input("Enter name : ")
+print("name = ",name)
+
