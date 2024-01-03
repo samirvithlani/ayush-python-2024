e9fbf032a446210953824d0c49913633328bea4d
samirvithlani <samir.vithlani83955@gmail.com>
2024-01-03 11:37:36 +0530

    if else

---
+++ decisionMaking/ifelse/ifdemo1.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age: "))
+
+if age>=18:
+    print("You are eligible to vote!!")
+    print("Please cast your vote!!")
+    print("Thank you for voting!!")
+    
+else:
+    print("You are not eligible to vote!!")        
+    print("Please try next year!!")
---
+++ decisionMaking/ifelse/ifdemo2.py
@@ -0,0 +1,3 @@
+#even odd
+#positive negative
+#3 nos find average of that nos avg > 50 thn pass else fail
