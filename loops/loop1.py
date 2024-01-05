a7caeb69bd3b8e6d82f64b03ed2a0aa93b236979
samirvithlani <samir.vithlani83955@gmail.com>
2024-01-05 11:11:39 +0530

    loops

---
+++ loops/loop1.py
@@ -0,0 +1,13 @@
+# for i in range(10): #0 to  9   
+#      print(i,end=" ")
+
+
+# for i in range(1,11):
+#     print(i,end=" ")
+
+# for i in range(1,30,3):
+#     print(i,end=" ")
+
+
+for i in range(10,0,-1):
+    print(i,end=" ")
