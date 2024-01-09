3300555de448e31264ba804bae05a7f7b798a966
samirvithlani <samir.vithlani83955@gmail.com>
2024-01-09 13:24:04 +0530

    string

---
+++ stirng/string3.py
@@ -0,0 +1,34 @@
+#all boolean methods...
+
+name = "Samir vithlani"
+
+x = name.isalnum()
+print(x)
+
+x = name.isalpha()
+print(x)
+
+
+x = name.isdigit()
+print(x)
+
+x = name.islower()
+print(x)
+
+x = name.isupper()
+print(x)
+
+x = name.isspace()
+print(x)
+
+
+x = name.istitle()
+print(x)
+
+message = "dont plant bomb in the school"
+x = message.startswith("dont ")
+print("starts with",x)
+x = message.endswith("s")
+print("ends with",x)
+
+
