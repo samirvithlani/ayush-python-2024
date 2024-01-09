9ed9d66b8bbd9ca6665d3dcc04d32712c90fb056
samirvithlani <samir.vithlani83955@gmail.com>
2024-01-09 13:45:13 +0530

    string

---
+++ stirng/string4.py
@@ -0,0 +1,13 @@
+message = "hi this is message from Amit"
+
+x = message.split(" ")
+print(x)
+
+address = "201,surbhi complex,opp,opp municiap market,navrangpura,ahmedabad"
+
+x = address.split(",")
+print(x)
+
+
+
+
---
+++ stirng/stringtask1.py
@@ -0,0 +1,18 @@
+sent ="hi this is @ $ ok India"
+
+count = 0
+for i in range(0,len(sent)):
+    if sent[i]==" ":
+        count+=1
+
+
+print("Number of words in the sentence is: ",count+1)        
+
+
+
+space =0
+for i in sent:
+    if i ==" ":
+        space+=1
+
+print("Number of words in the sentence is: ",space)        
