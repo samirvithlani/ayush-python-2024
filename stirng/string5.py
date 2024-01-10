19c4e0420ff0876e9aab14b07cad71096cc957c1
samirvithlani <samir.vithlani83955@gmail.com>
2024-01-10 11:19:29 +0530

    string

---
+++ stirng/string5.py
@@ -0,0 +1,19 @@
+user = "aamitmishra is good bowler"
+
+# ind = user.find("p")
+# print(ind)
+
+# ind1 = user.index("a")
+# print(ind1)
+
+#user = user.replace("a","@")
+user = user.replace("a","@",2)
+print(user)
+
+
+#cnt = user.count(" ")
+cnt = user.count(" ",5,12)
+print(cnt)
+
+
+
---
+++ stirng/string6.py
@@ -0,0 +1,10 @@
+email = "ssxssamirssxs"
+print(len(email))
+
+
+#email = email.strip()
+#email = email.lstrip()
+#email = email.rstrip()
+email  =  email.strip("s")
+print(len(email))
+print(email)
